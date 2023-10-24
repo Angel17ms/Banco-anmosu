9a2f0678b21c878eada107814416957b93cc9e74
vesprada <morenosuarezangel@gmail.com>
2023-10-24 15:10:51 +0200

    commit

--- app/src/main/java/com/example/banco_anmosu/LoginActivity.kt
+++ app/src/main/java/com/example/banco_anmosu/LoginActivity.kt
@@ -1,11 +1,61 @@
 package com.example.banco_anmosu
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import androidx.core.view.isEmpty
+import androidx.core.view.isNotEmpty
+import com.example.banco_anmosu.databinding.ActivityLoginBinding
+import com.google.android.material.snackbar.Snackbar
+import com.google.android.material.textfield.TextInputLayout
 
 class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_login)
+
+        binding = ActivityLoginBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.txtpass.onFocusChangeListener = View.OnFocusChangeListener { view, hasfocus ->
+            if (!hasfocus) {
+                if (binding.txtpass.text.toString().isEmpty()) {
+                    binding.Password.error = "Tienes que rellenar la contraseña"
+                }
+            }
+        }
+
+        binding.txtUsuario.onFocusChangeListener = View.OnFocusChangeListener { view, hasfocus ->
+            if (!hasfocus) {
+                if (binding.txtUsuario.text.toString().isEmpty()) {
+                    binding.usuario.error = "Tienes que rellenar el usuario"
+                }
+            }
+        }
+
+
+
+
+        binding.enviar.setOnClickListener {
+            val dni = binding.usuario.editText?.text.toString()
+
+            if (binding.txtUsuario.text.toString().isNotEmpty() && binding.txtpass.text.toString().isNotEmpty()){
+
+                    val intent = Intent(this, MainActivity::class.java)
+                    intent.putExtra("dni", dni)
+                    startActivity(intent)
+            }
+        }
+
+        binding.salir.setOnClickListener {
+            finish()
+        }
+
+
+
+
     }
 }
--- app/src/main/java/com/example/banco_anmosu/MainActivity.kt
+++ app/src/main/java/com/example/banco_anmosu/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.banco_anmosu
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.banco_anmosu.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val dni = intent.getStringExtra("dni")
+
+        binding.dni.text = "$dni"
+
     }
 }
